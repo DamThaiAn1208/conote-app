324e7605059442c5223fae3ee813e1821060306f
DamThaiAn1208 <aan123dz@gmail.com>
2026-04-20 01:05:51 +0700

    20-4:1H05AM:

--- conote-app/conote-client/src/main/java/com/conote/client/app/Launcher.java
+++ conote-app/conote-client/src/main/java/com/conote/client/app/Launcher.java
@@ -3,7 +3,6 @@ package com.conote.client.app;
 import javafx.application.Application;
 
 public class Launcher {
-//
     public static void main(String[] args) {
         Application.launch(ClientApplication.class, args);
     }
